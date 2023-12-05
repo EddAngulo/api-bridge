682ee69bc7ccced29edd1bc02d378e468ba0b910
Eduardo Angulo <edangulo@uninorte.edu.co>
2023-12-05 18:39:38 -0500

    Chore: Testing stuff

---
+++ src/app/api/test/route.ts
@@ -0,0 +1,11 @@
+import { type NextRequest, NextResponse } from "next/server";
+import { verifySignatureAppRouter } from "@upstash/qstash/dist/nextjs";
+
+async function handler(_req: NextRequest) {
+	await new Promise((r) => setTimeout(r, 1000));
+
+	console.log("Success");
+	return NextResponse.json({ name: "John Doe " });
+}
+
+export const POST = verifySignatureAppRouter(handler);
